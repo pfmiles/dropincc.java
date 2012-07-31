daee008c47c11532de75b210d2865901b2934907
pf_miles <miles.wy.1@gmail.com>
2012-07-31 17:00:55 +0800

    copyright added

--- src/main/java/com/github/pfmiles/dropincc/impl/llstar/GenedKleeneGruleType.java
+++ src/main/java/com/github/pfmiles/dropincc/impl/llstar/GenedKleeneGruleType.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 pf_miles.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     pf_miles - initial API and implementation
+ ******************************************************************************/
 package com.github.pfmiles.dropincc.impl.llstar;
 
 import com.github.pfmiles.dropincc.impl.GruleType;
