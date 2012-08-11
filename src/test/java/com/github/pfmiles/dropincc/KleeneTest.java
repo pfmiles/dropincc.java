13a2a02cb9120c747dacdb4b81491e00aa2e3826
pf_miles <miles.wy.1@gmail.com>
2012-08-11 16:59:24 +0800

    kleene tests added

--- src/test/java/com/github/pfmiles/dropincc/KleeneTest.java
+++ src/test/java/com/github/pfmiles/dropincc/KleeneTest.java
@@ -12,8 +12,10 @@ package com.github.pfmiles.dropincc;
 
 import junit.framework.TestCase;
 
+import com.github.pfmiles.dropincc.testhelper.IccActionManager;
+
 /**
- * Complex kleene nodes situation tests. // TODO NBK-NBK, NBK-BK, BK-NBK, BK-BK
+ * Complex kleene nodes situation tests.
  * 
  * @author pf-miles
  * 
@@ -21,6 +23,101 @@ import junit.framework.TestCase;
 public class KleeneTest extends TestCase {
 
     // NBK - NBK
+    /**
+     * <pre>
+     * S ::= A $
+     * A ::= (a (b d)+ c)*
+     *     | (e (g h)? f)+
+     * </pre>
+     */
     public void test1() {
+        IccActionManager mgr = new IccActionManager();
+        Lang lang = new Lang("Test");
+        Grule A = lang.newGrule();
+
+        lang.defineGrule(A, CC.EOF).action(mgr.newCheck(1));
+        A.define(CC.ks("a", CC.kc("b", "d")), "c").action(mgr.newCheck(0)).alt(CC.kc("e", CC.op("g", "h"), "f")).action(mgr.newCheck(1));
+
+        Exe exe = lang.compile();
+        exe.eval("efeghf");
+
+        mgr.checkFinalCounts();
+    }
+
+    // NBK - BK
+    /**
+     * <pre>
+     * S ::= A $
+     * A ::= (a (b C e)+ b C d)*
+     * C ::= f
+     *     | g C h
+     * </pre>
+     */
+    public void test2() {
+        IccActionManager mgr = new IccActionManager();
+        Lang lang = new Lang("Test");
+        Grule A = lang.newGrule();
+        Grule C = lang.newGrule();
+
+        lang.defineGrule(A, CC.EOF).action(mgr.newCheck(1));
+        A.define(CC.ks("a", CC.kc("b", C, "e"), "b", C, "d")).action(mgr.newCheck(1));
+        C.define("f").action(mgr.newCheck(7)).alt("g", C, "h").action(mgr.newCheck(11));
+
+        Exe exe = lang.compile();
+        exe.eval("abfebgfhebfdabggfhhebgggfhhhebggggfhhhhebgfhd");
+
+        mgr.checkFinalCounts();
+    }
+
+    // BK - NBK
+    /**
+     * <pre>
+     * S ::= A $
+     * A ::= (B (e f)+ d)* B c
+     * B ::= g
+     *     | h B i
+     * </pre>
+     */
+    public void test3() {
+        IccActionManager mgr = new IccActionManager();
+        Lang lang = new Lang("Test");
+        Grule A = lang.newGrule();
+        Grule B = lang.newGrule();
+
+        lang.defineGrule(A, CC.EOF).action(mgr.newCheck(1));
+
+        A.define(CC.ks(B, CC.kc("e", "f"), "d"), B, "c").action(mgr.newCheck(1));
+
+        B.define("g").action(mgr.newCheck(4)).alt("h", B, "i").action(mgr.newCheck(7));
+
+        Exe exe = lang.compile();
+
+        exe.eval("hgiefdhhgiiefefdhhhgiiiefefefdhgic");
+        mgr.checkFinalCounts();
+    }
+
+    // BK-BK
+    /**
+     * <pre>
+     * S ::= A $
+     * A ::= (B (B c)? B d)+ B c
+     * B ::= e
+     *     | f B g
+     * </pre>
+     */
+    public void test4() {
+        IccActionManager mgr = new IccActionManager();
+        Lang lang = new Lang("Test");
+        Grule A = lang.newGrule();
+        Grule B = lang.newGrule();
+
+        lang.defineGrule(A, CC.EOF).action(mgr.newCheck(1));
+        A.define(CC.kc(B, CC.op(B, "c"), B, "d"), B, "c").action(mgr.newCheck(1));
+        B.define("e").action(mgr.newCheck(6)).alt("f", B, "g").action(mgr.newCheck(15));
+
+        Exe exe = lang.compile();
+        exe.eval("efegdffeggfffegggcffffeggggdfffffegggggc");
+
+        mgr.checkFinalCounts();
     }
 }
